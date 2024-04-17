dcbfbf877166e9aea27f5ffdc69b03fd6ed551db
RamonSanGabriel <jeshero11@gmail.com>
2024-04-18 02:18:42 +0800

    update

---
+++ js/test.js
@@ -0,0 +1,17 @@
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Change code below this line
+  console.log const isMatch = '';
+
+  // let isValidPassword("mangodab3st");
+
+
+  if (isValidPassword === 'jqueryismyjam') {
+    isMatch = 'true';
+  } else if (isValidPassword !== 'jqueryismyjam') {
+    isMatch = 'false';
+  }
+
+  // Change code above this line
+  return isMatch;
+}
